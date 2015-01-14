5080cd6bc970ca83243de99b83f98c8df91773c8
manuntag <manuntag@ualberta.ca>
2015-01-13 19:00:10 -0800

    Fully functioning reward calc with response

--- Poppin Bottles/main.m
+++ Poppin Bottles/main.m
@@ -8,10 +8,42 @@
 
 #import <Foundation/Foundation.h>
 
+
+//For every two empty bottles, you can get one free (full) bottle of pop
+//For every four bottle caps, you can get one free (full) bottle of pop
+//Each bottle of pop costs $2 to purchase
+
+int bottles;
+int capsreward;
+int emptybottlesreward;
+int enteredInvestment;
+
+// function that will calculate # of redeemable rewards given the investment
+
+int calculateBottles (int investment) {
+    
+    bottles = investment/2;
+    capsreward = bottles/4;
+    emptybottlesreward = bottles/2;
+    
+    return capsreward + emptybottlesreward;
+    
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        
+        // user enters the amount they spent, function returns the number of free pops they will recieve
+        
+        NSLog(@"Find out how many FREE POPS you can redeem,\nEnter how much you spent!");
+        scanf("%d", &enteredInvestment);
+        int investment =enteredInvestment;
+        
+        int howManyRewards = calculateBottles(investment);
+        NSLog(@"\nYour $%d of Pop purchased %d bottles of pop,\nproduced %d free bottles through the caps rewards \nproduced %d free bottles through the empty bottles reward\nfor a total of %d free bottles!", investment, bottles, capsreward, emptybottlesreward,howManyRewards);
+        
+        
     }
     return 0;
 }

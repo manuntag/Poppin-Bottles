8deed976f1b1754fa32f61d0872323ff426217e0
manuntag <manuntag@ualberta.ca>
2015-01-13 16:58:48 -0800

    Initial Commit

---
+++ Poppin Bottles/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Poppin Bottles
+//
+//  Created by David Manuntag on 2015-01-13.
+//  Copyright (c) 2015 David Manuntag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
